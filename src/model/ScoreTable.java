378b7b4aa90051840ef872287169e4c61ef8cb02
Rony Farid Ordoñez García <ronyfarido@gmail.com>
2023-09-02 18:22:22 -0500

    Deleted method to insert players in score table..

--- src/model/ScoreTable.java
+++ src/model/ScoreTable.java
@@ -14,33 +14,5 @@ public class ScoreTable {
         root = newRoot;
     }
 
-    // Sin comprobar
-    public void insertPlayer(Player player) {
-        NodeST newNode = new NodeST(player);
-
-        if (root == null) {
-            root = newNode;
-        } else {
-            NodeST aux = root;
-            NodeST father;
-            while (true) {
-                father = aux;
-                if (player.getScore() < aux.getPlayer().getScore()) {
-                    aux = aux.getLeft();
-                    if (aux == null) {
-                        father.setLeft(newNode);
-                        return;
-                    }
-                } else {
-                    aux = aux.getRight();
-                    if (aux == null) {
-                        father.setRight(newNode);
-                        return;
-                    }
-                }
-            }
-        }
-    }
-
 
 }
